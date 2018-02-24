0930e2cc0541fd89678f052c93493cc59d79d6f3
Ruslan <20125922+rkrenzler@users.noreply.github.com>
2018-02-24 10:53:36 +0100

    Add LGPL note to the source files

--- frame.py
+++ frame.py
@@ -1,4 +1,28 @@
 # -*- coding: utf-8 -*-
+#***************************************************************************
+#*                                                                         *
+#*  This file is part of the Open Source Ecology D3D 3D Printer Workbench  *
+#*  for FreeCAD.                                                           *
+#*                                                                         *
+#*  Copyright (C) 2017                                                     *
+#*  Open Source Ecology <info|at|opensourceecology.org>                    *
+#*                                                                         *
+#*  This library is free software; you can redistribute it and/or          *
+#*  modify it under the terms of the GNU Lesser General Public             *
+#*  License as published by the Free Software Foundation; either           *
+#*  version 2 of the License, or (at your option) any later version.       *
+#*                                                                         *            
+#*  This library is distributed in the hope that it will be useful,        *
+#*  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+#*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+#*  Lesser General Public License for more details.                        *
+#*                                                                         *
+#*  You should have received a copy of the GNU Lesser General Public       *
+#*  License along with this library; if not, If not, see                   *
+#*  <http://www.gnu.org/licenses/>.                                        *
+#*                                                                         *
+#*                                                                         *
+#***************************************************************************
 # Author: Ruslan Krenzler.
 # Date: 11 February 2018
 # Create a pipe frame box.
