50568e8c4e53a7fc7768e443bf1c58587926d5f3
Ruslan <20125922+rkrenzler@users.noreply.github.com>
2018-02-24 08:49:54 +0100

    add comboboxes to the "add frame" command

--- frameGui.py
+++ frameGui.py
@@ -23,10 +23,22 @@ class MainDialog(QtGui.QDialog):
 		self.cornerTable = cornerTable
 		self.initUi()
 
+	def fillPipeNames(self):
+		for row in self.pipeTable.data:
+			name = row[self.pipeTable.nameIndex]
+			self.comboBoxPipeName.addItem(name)
+
+	def fillCornerNames(self):
+		for row in self.cornerTable.data:
+			name = row[self.cornerTable.nameIndex]
+			self.comboBoxCornerName.addItem(name)
+
 	def initUi(self): 
 		Dialog = self # Added 
 		self.result = -1 
 		self.setupUi(self)
+		self.fillPipeNames()
+		self.fillCornerNames()
 		# Restore previous user input. Ignore exceptions to prevent this part
 		# part of the code to prevent GUI from starting, once settings are broken.
 		try:
@@ -37,11 +49,10 @@ class MainDialog(QtGui.QDialog):
 		self.show()
 
 # The following lines are from QtDesigner .ui-file processed by pyside-uic
-# pyside-uic --indent=0 create-pipe.ui -o tmp.py
+# pyside-uic --indent=0 add-frame-box.ui -o tmp.py
 #
-# The file paths needs to be adjusted manually. For example
-# self.label.setPixmap(QtGui.QPixmap( GetMacroPath()+"/pipe-frame-box-dimensions.png"))
-# access datata in some special FreeCAD directory.
+# The file paths need to be adjusted manually. For example
+# os.path.join(D3DBase.IMAGE_PATH, "pipe-frame-box-dimensions.png")
 	def setupUi(self, Dialog):
 		Dialog.setObjectName("Dialog")
 		Dialog.resize(614, 566)
@@ -94,32 +105,22 @@ class MainDialog(QtGui.QDialog):
 		self.label_2 = QtGui.QLabel(self.horizontalWidget)
 		self.label_2.setGeometry(QtCore.QRect(0, 70, 91, 17))
 		self.label_2.setObjectName("label_2")
-		self.lineEditPipeName = QtGui.QLineEdit(self.horizontalWidget)
-		self.lineEditPipeName.setGeometry(QtCore.QRect(80, 70, 171, 27))
-		self.lineEditPipeName.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
-		self.lineEditPipeName.setObjectName("lineEditPipeName")
-		self.pushButtonSelectPipe = QtGui.QPushButton(self.horizontalWidget)
-		self.pushButtonSelectPipe.setEnabled(False)
-		self.pushButtonSelectPipe.setGeometry(QtCore.QRect(260, 70, 121, 27))
-		self.pushButtonSelectPipe.setObjectName("pushButtonSelectPipe")
-		self.pushButtonSelectCorner = QtGui.QPushButton(self.horizontalWidget)
-		self.pushButtonSelectCorner.setEnabled(False)
-		self.pushButtonSelectCorner.setGeometry(QtCore.QRect(260, 100, 121, 27))
-		self.pushButtonSelectCorner.setObjectName("pushButtonSelectCorner")
-		self.lineEditCornerName = QtGui.QLineEdit(self.horizontalWidget)
-		self.lineEditCornerName.setGeometry(QtCore.QRect(80, 100, 171, 27))
-		self.lineEditCornerName.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
-		self.lineEditCornerName.setObjectName("lineEditCornerName")
 		self.label_7 = QtGui.QLabel(self.horizontalWidget)
 		self.label_7.setGeometry(QtCore.QRect(0, 100, 91, 17))
 		self.label_7.setObjectName("label_7")
+		self.comboBoxPipeName = QtGui.QComboBox(self.horizontalWidget)
+		self.comboBoxPipeName.setGeometry(QtCore.QRect(90, 70, 221, 27))
+		self.comboBoxPipeName.setObjectName("comboBoxPipeName")
+		self.comboBoxCornerName = QtGui.QComboBox(self.horizontalWidget)
+		self.comboBoxCornerName.setGeometry(QtCore.QRect(110, 100, 201, 27))
+		self.comboBoxCornerName.setObjectName("comboBoxCornerName")
 		self.verticalLayout.addWidget(self.horizontalWidget)
-		self.label = QtGui.QLabel(Dialog)
-		self.label.setText("")
-		self.label.setPixmap(QtGui.QPixmap(os.path.join(D3DBase.IMAGE_PATH, "pipe-frame-box-dimensions.png")))
-		self.label.setAlignment(QtCore.Qt.AlignCenter)
-		self.label.setObjectName("label")
-		self.verticalLayout.addWidget(self.label)
+		self.labelDraft = QtGui.QLabel(Dialog)
+		self.labelDraft.setText("")
+		self.labelDraft.setPixmap(os.path.join(D3DBase.IMAGE_PATH, "pipe-frame-box-dimensions.png"))
+		self.labelDraft.setAlignment(QtCore.Qt.AlignCenter)
+		self.labelDraft.setObjectName("labelDraft")
+		self.verticalLayout.addWidget(self.labelDraft)
 		self.buttonBox = QtGui.QDialogButtonBox(Dialog)
 		self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
 		self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
@@ -132,20 +133,16 @@ class MainDialog(QtGui.QDialog):
 		QtCore.QMetaObject.connectSlotsByName(Dialog)
 
 	def retranslateUi(self, Dialog):
-		Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "Create frame box", None, QtGui.QApplication.UnicodeUTF8))
+		Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "Add frame", None, QtGui.QApplication.UnicodeUTF8))
 		self.checkBoxCreateSolid.setText(QtGui.QApplication.translate("Dialog", "Create Solid", None, QtGui.QApplication.UnicodeUTF8))
 		self.label_3.setText(QtGui.QApplication.translate("Dialog", "LX:", None, QtGui.QApplication.UnicodeUTF8))
-		self.lineEditLX.setText(QtGui.QApplication.translate("Dialog", "12 in", None, QtGui.QApplication.UnicodeUTF8))
+		self.lineEditLX.setText(QtGui.QApplication.translate("Dialog", "30 cm", None, QtGui.QApplication.UnicodeUTF8))
 		self.label_5.setText(QtGui.QApplication.translate("Dialog", "LY:", None, QtGui.QApplication.UnicodeUTF8))
-		self.lineEditLY.setText(QtGui.QApplication.translate("Dialog", "8 in", None, QtGui.QApplication.UnicodeUTF8))
+		self.lineEditLY.setText(QtGui.QApplication.translate("Dialog", "20 cm", None, QtGui.QApplication.UnicodeUTF8))
 		self.label_6.setText(QtGui.QApplication.translate("Dialog", "LZ:", None, QtGui.QApplication.UnicodeUTF8))
-		self.lineEditLZ.setText(QtGui.QApplication.translate("Dialog", "4 in", None, QtGui.QApplication.UnicodeUTF8))
+		self.lineEditLZ.setText(QtGui.QApplication.translate("Dialog", "40 cm", None, QtGui.QApplication.UnicodeUTF8))
 		self.label_2.setText(QtGui.QApplication.translate("Dialog", "Pipe name:", None, QtGui.QApplication.UnicodeUTF8))
-		self.lineEditPipeName.setText(QtGui.QApplication.translate("Dialog", "NPS 1 in PVC SCH 40", None, QtGui.QApplication.UnicodeUTF8))
-		self.pushButtonSelectPipe.setText(QtGui.QApplication.translate("Dialog", "Select Pipe", None, QtGui.QApplication.UnicodeUTF8))
-		self.pushButtonSelectCorner.setText(QtGui.QApplication.translate("Dialog", "Select Corner", None, QtGui.QApplication.UnicodeUTF8))
-		self.lineEditCornerName.setText(QtGui.QApplication.translate("Dialog", "F0013WE", None, QtGui.QApplication.UnicodeUTF8))
-		self.label_7.setText(QtGui.QApplication.translate("Dialog", "Pipe name:", None, QtGui.QApplication.UnicodeUTF8))
+		self.label_7.setText(QtGui.QApplication.translate("Dialog", "Corner name:", None, QtGui.QApplication.UnicodeUTF8))
 
 	def accept(self):
 		"""User clicked OK"""
@@ -181,13 +178,13 @@ class MainDialog(QtGui.QDialog):
 			msgBox.exec_()
 			return
 	
-		pipeName = self.lineEditPipeName.text()
+		pipeName = self.comboBoxPipeName.currentText()
 		if pipeName == "":
 			msgBox = QtGui.QMessageBox()
 			msgBox.setText("Enter pipe name.")
 			msgBox.exec_()
 			return
-		cornerName = self.lineEditCornerName.text()
+		cornerName = self.comboBoxCornerName.currentText()
 		if cornerName == "":
 			msgBox = QtGui.QMessageBox()
 			msgBox.setText("Enter corner name.")
@@ -202,7 +199,6 @@ class MainDialog(QtGui.QDialog):
 		# Call parent class.
 		super(MainDialog, self).accept()
 
-
 	def saveInput(self):
 		"""Store user input for the next run."""
 		settings = QtCore.QSettings(MainDialog.QSETTINGS_APPLICATION, MainDialog.QSETTINGS_NAME)
@@ -211,9 +207,21 @@ class MainDialog(QtGui.QDialog):
 		settings.setValue("lineEditLX", self.lineEditLX.text())
 		settings.setValue("lineEditLY", self.lineEditLY.text())
 		settings.setValue("lineEditLZ", self.lineEditLZ.text())
-		settings.setValue("lineEditPipeName", self.lineEditPipeName.text())
-		settings.setValue("lineEditCornerName", self.lineEditCornerName.text())
+		settings.setValue("comboBoxPipeName", self.comboBoxPipeName.currentText())
+		settings.setValue("comboBoxCornerName", self.comboBoxCornerName.currentText())
 		settings.sync()
+
+	@staticmethod
+	def selectOrAdd(combobox, text):
+		""" Select text in combobox if it exists. If not add it.
+		We usually whant to keep old text, in order not to lose its value when
+		the dialog is closed. Even if this text can be temporary not be invalid, because the corresponding
+		part table is incomplete.
+		"""
+		index = combobox.findText(text, QtCore.Qt.MatchFixedString)
+		if index < 0: # Item not found, add it.
+			index = combobox.addItem(text)
+		combobox.setCurrentIndex(index)
 
 	def restoreInput(self):
 		settings = QtCore.QSettings(MainDialog.QSETTINGS_APPLICATION, MainDialog.QSETTINGS_NAME)
@@ -228,14 +236,12 @@ class MainDialog(QtGui.QDialog):
 		text = settings.value("lineEditLZ")
 		if text is not None:
 			self.lineEditLZ.setText(text)
-		text = settings.value("lineEditPipeName")
+		text = settings.value("comboBoxPipeName")
 		if text is not None:
-			self.lineEditPipeName.setText(text)
-		text = settings.value("lineEditCornerName")
+			self.selectOrAdd(self.comboBoxPipeName, text)
+
+		text = settings.value("comboBoxCornerName")
 		if text is not None:
-			self.lineEditCornerName.setText(text)
-
-
-
+			self.selectOrAdd(self.comboBoxCornerName, text)
 
 
